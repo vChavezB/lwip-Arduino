015f3dae1e72b87f6f12afe2b3df2b9b31672daf
Victor Chavez <chavez-bermudez@fh-aachen.de>
2022-04-29 18:11:20 +0200

    Fixed relative dir for netif and deleted Makefs

--- src/src/include/netif/slipif.h
+++ src/src/include/netif/slipif.h
@@ -41,7 +41,7 @@
 #define LWIP_HDR_NETIF_SLIPIF_H
 
 #include "../lwip/opt.h"
-#include "netif.h"
+#include "../lwip/netif.h"
 
 /** Set this to 1 to start a thread that blocks reading on the serial line
  * (using sio_read()).
